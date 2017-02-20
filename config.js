dd1e690354b2c9cd9c37096fc525d21299250e27
Raynaldo <raynaldoadp@gmail.com>
2017-02-20 09:29:57 +0000

    added landing page

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 var mLab_Url = 'mongodb://raynaldoadp:raynaldoadp@ds139567.mlab.com:39567/raynaldodb';
 
+
 exports.DATABASE_URL = mLab_Url ||
                        process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
